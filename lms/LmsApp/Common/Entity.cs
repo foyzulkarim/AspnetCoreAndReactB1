7a0a92111dee621ebeda7aa8b4129f271c2a2b0c
Foyzul Karim <foyzulkarim@gmail.com>
2018-04-18 22:35:40 +0600

    database creation ok

---
+++ lms/LmsApp/Common/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Common
+{
+    public class Entity
+    {
+        public string Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime Modified { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+        public bool IsActive { get; set; } // can be toggled
+        
+        public bool IsDeleted { get; set; } // can not be toggled
+        
+    }
+}
---
+++ lms/LmsApp/Model/LmsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Model
+{
+    public class LmsDbContext : DbContext
+    {
+        public LmsDbContext(DbContextOptions options): base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().HasIndex(x => x.Name);
+            modelBuilder.Entity<Course>().HasIndex(x => x.Topic);
+            modelBuilder.Entity<Course>().HasIndex(x => x.IsFree);
+        }
+    }
+}
