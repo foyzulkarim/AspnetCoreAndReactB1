602d505e3ceff40b0775c0d7e54d782f10dc3ef9
Foyzul Karim <foyzulkarim@gmail.com>
2018-04-18 23:01:05 +0600

    request model added

---
+++ lms/LmsApp/Common/ViewModel/DropdownViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.ViewModel
+{
+    public class DropdownViewModel
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+
+        public object Data { get; set; }
+    }
+}
---
+++ lms/LmsApp/RequestModel/CourseRequestModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Common.RequestModel;
+using Common.ViewModel;
+using Model;
+
+namespace RequestModel
+{
+    public class CourseRequestModel : BaseRequestModel<Course>
+    {
+
+        public CourseRequestModel(string keyword, string orderBy = "Modified", string isAscending = "False") : base(keyword, orderBy, isAscending)
+        {
+
+        }
+
+        protected override Expression<Func<Course, bool>> GetExpression()
+        {
+            ExpressionObj = x => x.Name.Contains(Keyword) || x.Topic.Contains(Keyword);
+            ExpressionObj = ExpressionObj.And(GenerateBaseEntityExpression());
+            return ExpressionObj;
+        }
+
+        public override Expression<Func<Course, DropdownViewModel>> Dropdown()
+        {
+            return x => new DropdownViewModel() {Id = x.Id, Text = x.Name, Data = x};
+        }
+    }
+}
